af2ac482af3e411acace857b05fefc2d8e6351ff
ypupreti <85892615+ypupreti@users.noreply.github.com>
2024-03-13 12:47:43 +0530

    Update prep_data.py

--- prep_data.py
+++ prep_data.py
@@ -1,4 +1,16 @@
 from datasets import load_dataset
 dataset_name = "ai4bharat/indic-instruct-data-v0.1"
 dataset = load_dataset(dataset_name, 'anudesh')
-dataset['hi']
+#dataset['hi']
+
+
+text = dataset['hi'][0]['messages']
+que = text[0]['content']
+res = text[1]['content']
+#dt = f"<s>[INST] "+{que}+"[/INST] "{res}+" </s>"
+dt = f"<s>[INST] {que} [/INST] {res} </s>"
+formated_data = {}
+formated_data['text']=dt
+text_list = []
+text_list.append(formated_data)
+print(text_list)
